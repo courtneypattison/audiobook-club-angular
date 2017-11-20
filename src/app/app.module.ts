18840a9bcd8bca87a8b618d582afecaad512af32
courtneypattison <courtney@courtneypattison.com>
2017-11-20 14:26:14 -0800

    Added HttpClientModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 
@@ -12,7 +13,8 @@ import { AudiobooksComponent } from './audiobooks/audiobooks.component';
     AudiobooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
