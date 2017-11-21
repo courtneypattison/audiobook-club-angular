f12d1ba25b56def613ca885bca6bb2a9f5e6cfad
courtneypattison <courtney@courtneypattison.com>
2017-11-21 13:49:41 -0800

    Added properties to audiobook model

--- src/app/audiobooks/shared/audiobook.model.ts
+++ src/app/audiobooks/shared/audiobook.model.ts
@@ -1,4 +1,13 @@
+import { Chapter } from './chapter.model';
+
 export class Audiobook {
-    id: string;
+    identifier: string;
     title?: string;
+    authors?: string[];
+    description?: string;
+    subjects?: string;
+    length?: string;
+    rating?: string;
+    imageURL?: string;
+    chapters?: Chapter[];
 }
