dbbcdf0865c7003eb7708fb78c56d1732b9093f1
courtneypattison <courtney@courtneypattison.com>
2017-11-28 09:39:55 -0800

    Removed broken app level e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,11 @@
 import { AppPage } from './app.po';
 
+
 describe('audiobook-club-angular App', () => {
   let page: AppPage;
 
+
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
