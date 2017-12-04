ca2453d96b444d857117d3ce4d4e131045affc92
courtneypattison <courtney@courtneypattison.com>
2017-12-04 11:04:23 -0800

    Added RouterTestingModule to audiobooks test

--- src/app/audiobooks/audiobooks.component.spec.ts
+++ src/app/audiobooks/audiobooks.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AudiobooksComponent } from './audiobooks.component';
 
@@ -8,6 +9,7 @@ describe('AudiobooksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AudiobooksComponent]
     })
     .compileComponents();
