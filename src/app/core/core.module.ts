9c75c208a1220ba59bf7cf6ddafbe7d9a9a97fc5
courtneypattison <courtney@courtneypattison.com>
2017-12-05 15:13:53 -0800

    Generated HeaderComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavComponent]
+  exports: [NavComponent],
+  declarations: [NavComponent, HeaderComponent]
 })
 export class CoreModule {
   constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
