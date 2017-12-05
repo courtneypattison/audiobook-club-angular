d1a3c7be0ec8ea1fd0302eea49e571b12cde7480
courtneypattison <courtney@courtneypattison.com>
2017-12-05 15:18:12 -0800

    Generated FooterComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,14 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [HeaderComponent],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class CoreModule {
   constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
