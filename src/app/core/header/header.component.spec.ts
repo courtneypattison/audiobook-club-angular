32b2943bf3569ca76947b76bed3850603e26e427
courtneypattison <courtney@courtneypattison.com>
2017-12-06 11:15:55 -0800

    Added HeaderComponent title test

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let debug: DebugElement;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,19 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+
+    debug = fixture.debugElement.query(By.css('header'));
+    element = debug.nativeElement;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the header', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display title', () => {
+    fixture.detectChanges();
+    expect(element.textContent).toContain(component.title);
   });
 });
