651035302abdd4e8c4bbf4614f77a52e4f0b16ee
courtneypattison <courtney@courtneypattison.com>
2017-12-07 16:27:36 -0800

    Added test for getting an Observable<Audiobook[]> back from getAudiobooks()

--- src/app/audiobooks/shared/audiobook.service.spec.ts
+++ src/app/audiobooks/shared/audiobook.service.spec.ts
@@ -1,19 +1,55 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientJsonpModule, HttpBackend, JsonpClientBackend } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { Audiobook } from './audiobook.model';
 import { AudiobookService } from './audiobook.service';
 
 describe('AudiobookService', () => {
+  let audiobookService: AudiobookService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
       ],
-      providers: [AudiobookService]
+      providers: [
+        AudiobookService,
+        { provide: JsonpClientBackend, useExisting: HttpBackend }
+      ]
     });
+
+    audiobookService = TestBed.get(AudiobookService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([AudiobookService], (service: AudiobookService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('#getAudiobooks', () => {
+    it('should return an Observable<Audiobook[]>', () => {
+      const dummyIdentifiers = {
+        'response': {
+          'docs': [
+            {'identifier': 'tom_sawyer_librivox'},
+            {'identifier': 'moby_dick_librivox'},
+            {'identifier': 'adventures_holmes'},
+            {'identifier': 'alice_in_wonderland_librivox'},
+            {'identifier': 'art_of_war_librivox'}
+          ]
+        }
+      };
+
+      audiobookService.getAudiobooks().subscribe(audiobooks => {
+        expect(audiobooks.length).toBe(5);
+        expect(audiobooks[0].identifier).toEqual(dummyIdentifiers['response']['docs'][0]['identifier']);
+      });
+
+      const req = httpMock.expectOne(request => request.url === audiobookService.getAudiobooksUrl());
+      expect(req.request.method).toBe('JSONP');
+      req.flush(dummyIdentifiers);
+    });
+  });
 });
